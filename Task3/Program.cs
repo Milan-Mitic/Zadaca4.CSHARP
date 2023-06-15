a3b71b21ec2c26e27dc757774566730449857346
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-15 12:08:31 +0200

    Add files via upload

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sumOfDigits = SumOfDigits(number);
+            Console.WriteLine("Sum of digits: " +  sumOfDigits);
+        }
+
+        static int SumOfDigits(int number)
+        {
+            number = Math.Abs(number);
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += digit;  
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
