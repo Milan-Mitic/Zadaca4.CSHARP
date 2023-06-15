044193f8efbf46451e66760e75b906407fcab198
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-15 12:07:36 +0200

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter sentence: ");
+            string sentenceInput = Console.ReadLine();
+
+            string[] words = sentenceInput.Split(' ');
+
+            Console.WriteLine("Words in the sentence:\n ");
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
