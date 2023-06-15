0b39addc2b01a4cf2a44c78335a14b36aa35cab4
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-15 12:06:33 +0200

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter one word: ");
+            string wordInput = Console.ReadLine();
+           
+
+            if (wordInput.Length <= 5)
+            {
+                Console.WriteLine("The word is too short !!!");
+            } else
+            {
+                string lastFiveChars = wordInput.Substring(wordInput.Length - 5);
+                Console.WriteLine("Last 5 characters: " + lastFiveChars);
+            }
+            
+        }
+    }
+}
